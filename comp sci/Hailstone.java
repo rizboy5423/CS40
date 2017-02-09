e80d4a1c2492ce094bdae411aee98ede19c4da6e
rizboy5423 <rizboy5423@gmail.com>
2017-02-08 22:30:56 -0600

    Add files via upload

---
+++ comp sci/Hailstone.java
@@ -0,0 +1,35 @@
+	import java.util.*;
+
+public class Hailstone
+
+{
+    public static void main(String[] arg)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("n: ");
+        int num = in.nextInt();
+        int line = 0;
+        while(num !=1){
+        if(num % 2 == 0)
+        {
+           
+            System.out.print(num + " ");
+             num = num / 2;
+            line++;
+        }
+        if(num % 2 == 1)
+        {
+            
+            System.out.print(num + " ");
+            num = num * 3 + 1;
+            line++;
+        }
+        if(line % 10 == 0)
+        {
+            System.out.println();
+            line=0;
+        }
+       
+    }
+}
+}
